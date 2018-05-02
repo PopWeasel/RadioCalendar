aadb35f28aa0f758cad160cc266fa47b3e64db61
PopWeasel <matloob.qureshi@gmail.com>
2018-05-02 23:15:11 +0100

    added fetch client and started handling query in server

--- client/src/Calendar/schedule/Week.js
+++ client/src/Calendar/schedule/Week.js
@@ -2,6 +2,35 @@ import React, {Component} from 'react';
 import moment from 'moment';
 
 class Week extends Component {
+  constructor(props) {
+    super(props);
+    moment.locale('en-GB');
+    this.state =  {
+      config: require('Config'),
+      events: [],
+      days: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log("Change" + event);
+    this.props.onEventsChange(event.target.value);
+  }
+
+  componentDidMount() {
+    const date = moment(this.props.selectedDate);
+    const weekOfYear = date.format("ww");
+    const year = date.format("YYYY");
+    const selectedStation = this.props.selectedStation;
+    const server = this.state.config.client.proxy;
+    const port = this.state.config.server.port;
+    const url = `http://${server}:${port}/station/${selectedStation.key}/year/${year}/week/${weekOfYear}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({events: data.events}));
+  }
+
   render() {
     const date = moment(this.props.selectedDate);
     const weekOfYear = date.format("ww");
@@ -17,10 +46,16 @@ class Week extends Component {
         </div>
         <div>
           Year : {year}
-        </div><div>
+        </div>
+        <div>
           Week of year : {weekOfYear}
         </div>
-
+        <div>
+          Events : {this.props.selectedEvents}
+        </div>
+        <div>
+          All Events : {this.state.events}
+        </div>
 
       </div>
     );
--- server/src/CalendarApp.js
+++ server/src/CalendarApp.js
@@ -2,14 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.json(app.locals.config);
 });
 
 app.get("/station/:station/year/:year/week/:weekOfYear", function(req, res) {
   const availableStations = app.locals.config.stations;
+  const selectedStation = req.params.station;
+  const year = req.params.year;
+  const week = req.params.weekOfYear;
+  console.log(`${selectedStation} => ${year}:${week}`);
+  const data =  {
+      "events": [1,2,3,4,5,6,7,8]
+  }
 
-  res.json("getting rest values");
+  res.json(data);
 })
 
 module.exports = app;
