6271875c4815f3426fd72e94ce0546183738fbfd
PopWeasel <matloob.qureshi@gmail.com>
2018-06-18 23:41:29 +0100

    partial implementation of click toggle

--- client/src/Calendar/schedule/Event.js
+++ client/src/Calendar/schedule/Event.js
@@ -6,6 +6,21 @@ import Typography from '@material-ui/core/Typography';
 import withTheme from '@material-ui/core/styles/withTheme';
 
 class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: {}
+    }
+  }
+
+  toggleEventActive = (e, event) => {
+    console.log(`State {this.state.active}`);
+    const active = this.state.active ? false : true;
+    this.setState({
+      active: {}
+    });
+  };
+
   render() {
     let column;
     let startRow;
@@ -23,18 +38,26 @@ class Event extends Component {
       const start = moment(event.start);
       const end = moment(event.end);
 
+      const title = <Typography variant="title">{event.title}</Typography>;
+      const subtitle = <Typography variant="subheading">{event.subTitle}</Typography>;
+      const synopsis = <Typography variant="body1">{event.synopsis}</Typography>;
+      const time = <Typography variant="body1">{start.format("HH:mm")} - {end.format("HH:mm")}</Typography>;
+      let episode = "";
+      if ('total' in event && 'episode' in event) {
+        episode = <Typography variant="body2">{event.episode}/{event.total}</Typography>
+      }
+      const pid = <Typography variant="body1">{event.pid}</Typography>;
+      //const className = this.state.active ? 'selected' : 'unselected';
       events.push(
-        <div>
-          <Typography>{event.title}</Typography>
-          <Typography>{event.subTitle}</Typography>
-          <Typography>{event.synopsis}</Typography>
-          <Typography>{start.format("HH:mm")}</Typography>
-          <Typography>{end.format("HH:mm")}</Typography>
-          <Typography>{event.pid}</Typography>
+        <div onClick={e => this.toggleEventActive(e, event)}>
+          {title}
+          {subtitle}
+          {episode}
+          {synopsis}
+          {time}
+          {pid}
         </div>
       );
-
-
     }
 
     //console.log(`${event.title} Times: ${start.format("HH:mm")} - ${end.format("HH:mm")} => ${startRow} - ${endRow}`);
